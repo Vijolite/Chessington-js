b0b4220d89f30ddc8b9e6de3d706f12980867e93
Ija Shaporenkova <ija.sha@gmail.com>
2022-01-25 16:11:03 +0000

    added testing of rook, bishop, king

--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,44 @@
+// import Queen from '../../../src/engine/pieces/queen';
+
+// describe('Queen', () => {
+
+// });
+
+
+import 'chai/register-should';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 import Queen from '../../../src/engine/pieces/queen';
 
-describe('Queen', () => {
+describe('Bishop', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move diagonally', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(2, 5), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        const expectedMoves = [
+            
+            Square.at(1, 6), Square.at(0, 7), Square.at(3, 4), Square.at(4, 3), Square.at(5, 2), Square.at(6, 1), Square.at(7, 0),
+            Square.at(1, 4), Square.at(0, 3), Square.at(3, 6), Square.at(4, 7)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+   
+
+    it('cannot make any other moves', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(2, 5), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.have.length(11);
+    });
 });
